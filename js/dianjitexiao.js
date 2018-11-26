6ed70fe4604bbf31e1faaf753ff4bf7870ce8b48
witheredstar <witheredstar@163.com>
2018-11-26 16:43:12 +0800

    Site updated: 2018-11-26 16:43:11

--- js/dianjitexiao.js
+++ js/dianjitexiao.js
@@ -1,75 +1,47 @@
-onload = function() {
-    var click_cnt = 0;
-    var $html = document.getElementsByTagName("html")[0];
-    var $body = document.getElementsByTagName("body")[0];
-    $html.onclick = function(e) {
-        var $elem = document.createElement("b");
-        $elem.style.color = "#E94F06";
-        $elem.style.zIndex = 9999;
-        $elem.style.position = "absolute";
-        $elem.style.select = "none";
-        var x = e.pageX;
-        var y = e.pageY;
-        $elem.style.left = (x - 10) + "px";
-        $elem.style.top = (y - 20) + "px";
-        clearInterval(anim);
-        switch (++click_cnt) {
-            case 10:
-                $elem.innerText = "OωO";
-                break;
-            case 20:
-                $elem.innerText = "(๑•́ ∀ •̀๑)";
-                break;
-            case 30:
-                $elem.innerText = "(๑•́ ₃ •̀๑)";
-                break;
-            case 40:
-                $elem.innerText = "(๑•̀_•́๑)";
-                break;
-            case 50:
-                $elem.innerText = "（￣へ￣）";
-                break;
-            case 60:
-                $elem.innerText = "(╯°口°)╯(┴—┴";
-                break;
-            case 70:
-                $elem.innerText = "૮( ᵒ̌皿ᵒ̌ )ა";
-                break;
-            case 80:
-                $elem.innerText = "╮(｡>口<｡)╭";
-                break;
-            case 90:
-                $elem.innerText = "( ง ᵒ̌皿ᵒ̌)ง⁼³₌₃";
-                break;
-            case 100:
-			    $elem.innerText = "😝";
-                break;
-            case 101:
-			    $elem.innerText = "😐";
-                break;
-            case 102:
-            case 103:
-            case 104:
-            case 105:
-                $elem.innerText = "(ꐦ°᷄д°᷅)";
-                break;
-            default:
-                $elem.innerText = "❤";
-                break;
+!function(e, t, a) {
+    function r() {
+        for (var e = 0; e < s.length; e++) s[e].alpha <= 0 ? (t.body.removeChild(s[e].el), s.splice(e, 1)) : (s[e].y--, s[e].scale += .004, s[e].alpha -= .013, s[e].el.style.cssText = "left:" + s[e].x + "px;top:" + s[e].y + "px;opacity:" + s[e].alpha + ";transform:scale(" + s[e].scale + "," + s[e].scale + ") rotate(45deg);background:" + s[e].color + ";z-index:99999");
+        requestAnimationFrame(r)
+    }
+    function n() {
+        var t = "function" == typeof e.onclick && e.onclick;
+        e.onclick = function(e) {
+            t && t(),
+            o(e)
         }
-        $elem.style.fontSize = Math.random() * 10 + 8 + "px";
-        var increase = 0;
-        var anim;
-        setTimeout(function() {
-            anim = setInterval(function() {
-                if (++increase == 150) {
-                    clearInterval(anim);
-                    $body.removeChild($elem);
-                }
-                $elem.style.top = y - 20 - increase + "px";
-                $elem.style.opacity = (150 - increase) / 120;
-            }, 8);
-        }, 70);
-        $body.appendChild($elem);
-    };
-};
+    }
+    function o(e) {
+        var a = t.createElement("div");
+        a.className = "heart",
+        s.push({
+            el: a,
+            x: e.clientX - 5,
+            y: e.clientY - 5,
+            scale: 1,
+            alpha: 1,
+            color: c()
+        }),
+        t.body.appendChild(a)
+    }
+    function i(e) {
+        var a = t.createElement("style");
+        a.type = "text/css";
+        try {
+            a.appendChild(t.createTextNode(e))
+        } catch(t) {
+            a.styleSheet.cssText = e
+        }
+        t.getElementsByTagName("head")[0].appendChild(a)
+    }
+    function c() {
+        return "rgb(" + ~~ (255 * Math.random()) + "," + ~~ (255 * Math.random()) + "," + ~~ (255 * Math.random()) + ")"
+    }
+    var s = [];
+    e.requestAnimationFrame = e.requestAnimationFrame || e.webkitRequestAnimationFrame || e.mozRequestAnimationFrame || e.oRequestAnimationFrame || e.msRequestAnimationFrame ||
+    function(e) {
+        setTimeout(e, 1e3 / 60)
+    },
+    i(".heart{width: 10px;height: 10px;position: fixed;background: #f00;transform: rotate(45deg);-webkit-transform: rotate(45deg);-moz-transform: rotate(45deg);}.heart:after,.heart:before{content: '';width: inherit;height: inherit;background: inherit;border-radius: 50%;-webkit-border-radius: 50%;-moz-border-radius: 50%;position: fixed;}.heart:after{top: -5px;}.heart:before{left: -5px;}"),
+    n(),
+    r()
+} (window, document);
